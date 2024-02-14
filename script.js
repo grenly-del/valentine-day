b46f74658fb3e81433c75c5fbdb14d8037346bb5
granly-del <ardiand400@gmail.com>
2024-02-14 09:39:59 +0800

    merubah pesan terakhir

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 teksjudulakhir2 = "I Love You ✨";
 pesanWA = "Iya aku sayang kamu "; // Misal 100%!
 
+
+const nilai = document.querySelector('.inirange')
 ////////
+
+console.log(nilai)
   
 const body = document.querySelector("body"); const swalst = Swal.mixin({timer: 2777, allowOutsideClick: false, showConfirmButton: false, timerProgressBar: true, imageHeight: 90,}); initom.style="opacity:0;bottom:0;transition:none"; audio = new Audio('' + linkmp3.src); function berjatuhan() {const heart = document.createElement("div"); heart.className = "fas fa-heart"; heart.style.left = (Math.random() * 90)+"vw"; heart.style.animationDuration = (Math.random()*3)+2+"s"; body.appendChild(heart);} setInterval(function name(params) {var heartArr = document.querySelectorAll(".fa-heart"); if (heartArr.length > 100) {heartArr[0].remove()}},100);
